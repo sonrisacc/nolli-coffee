ef7d7cc6454dff75d1fa5041f3efed2b7e09fad9
Sonrisa <sonrisa722611@gmail.com>
2017-10-18 19:19:39 -0700

    Update test with shallow testing

--- src/__tests__/BeanList.spec.jsx
+++ src/__tests__/BeanList.spec.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-// import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+// import renderer from 'react-test-renderer';
 import BeanList from '../BeanList';
 
 test('BeanList renders correctly', () => {
-  const component = renderer.create(<BeanList />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  // const component = renderer.create(<BeanList />);
+  // const tree = component.toJSON();
+  // expect(tree).toMatchSnapshot();
+  const component = shallow(<BeanList />);
+  expect(component).toMatchSnapshot();
 });
