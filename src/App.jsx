e3b9b9a683534c642cac6ce29203ce96c4189933
Sonrisa <sonrisa722611@gmail.com>
2017-10-20 16:20:19 -0700

    Update hot module replace config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import BeanList from './BeanList';
@@ -17,4 +16,4 @@ const App = () => (
   </BrowserRouter>
 );
 
-render(<App />, document.getElementById('app'));
+export default App;
